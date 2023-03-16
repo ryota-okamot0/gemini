c5a052412307a0d91a4694e376c12d0b87dbba5a
ryota.okamoto <r.okamoto@ridecore.co.jp>
2023-03-17 00:51:14 +0900

    commit

---
+++ app/Controllers/Controller.class.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace app\Controller;
+
+require_once dirname ( __FILE__ ) . '/../../vendor/autoload.php';
+require_once dirname ( __FILE__ ) . '/../utility/utility.php';
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Smarty;
+use app\utility\utility;
+
+/**
+ * コントローラ基底クラス
+ * Controller
+ *
+ * コントローラの基底クラス
+ */
+class Controller
+{
+    private $name   = 'controller';
+    protected function __construct(){
+        // コントローラーを直接呼ばれてもnewできないように
+    }
+    // ログの出力
+    public function logging($message, string $file_name = 'app.log'){
+        $Logger = new Logger('logger');
+        $Logger->pushHandler(new StreamHandler(__DIR__ . '/../../logs/' . $file_name, Logger::INFO));
+        $Logger->addInfo($message);
+    }
+    // ビューの生成
+    public function view(string $template, array $param): string{
+        $Smarty = new Smarty();
+        $Smarty->template_dir = __DIR__ . '/../view/';
+        $Smarty->compile_dir  = __DIR__ . '/../view/view_c/';
+        $Smarty->escape_html  = true;
+        $Smarty->assign([
+            'cssUnCache'    => Utility::cssUnCache()
+        ]);
+        $Smarty->assign($param);
+        return $Smarty->fetch($template . '.tpl');
+    }
+}
--- webroot/index.php
+++ webroot/index.php
@@ -2,3 +2,14 @@
 
 // https://uidev.jp/entry-71.html
 echo 'hello';
+
+$arrayParseUri = explode('/', $_SERVER['REQUEST_URI']);
+$lastUri = end($arrayParseUri);
+$call = substr($lastUri, 0, strcspn($lastUri,'?'));
+
+
+if ($call === "") {
+    // $call = strtoupper()
+}
+var_dump($call);
+exit;
