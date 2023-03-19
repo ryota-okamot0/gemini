6b4f14c03577c4a8fe1e17b4c618756ffc76a474
ryota.okamoto <r.okamoto@ridecore.co.jp>
2023-03-19 23:13:42 +0900

    Add Index

--- webroot/index.php
+++ webroot/index.php
@@ -1,15 +1,39 @@
 <?php
-
+// http://localhost:8080/
 // https://uidev.jp/entry-71.html
-echo 'hello';
+// https://www.blog.danishi.net/2019/05/29/post-1292/
+require_once '../config/constants.php';
 
-$arrayParseUri = explode('/', $_SERVER['REQUEST_URI']);
+echo 'Hello Gemini!';
+
+// URLを取得
+$arrayParseUri = explode(SYMBOL_SLASH, $_SERVER['REQUEST_URI']);
 $lastUri = end($arrayParseUri);
-$call = substr($lastUri, 0, strcspn($lastUri,'?'));
+$call = substr($lastUri, 0, strcspn($lastUri, SYMBOL_QUESTION));
 
-
+// URLからアクションが取得できない場合はIndexを参照する
 if ($call === "") {
-    // $call = strtoupper()
+    $call = "Index";
 }
-var_dump($call);
-exit;
+
+// URLと同名のControllerクラスを探しに行く
+if (file_exists('../'. DIR_CONTROLLERS. $call. 'class.php')) {
+    // 見つかったファイルをインクルードしてコントローラーをインスタンス化
+    include('../'. DIR_CONTROLLERS. $call. 'class.php');
+    $class = DIR_CONTROLLERS . $call;
+    $obj   = new $class();
+    
+    if ($_SERVER["REQUEST_METHOD"] != "POST") {
+        // GETならindexメソッドを呼び出す
+        $response = $obj->index();
+    } else {
+        // POSTならpostメソッドを呼び出す
+        $response = $obj->post();
+    }
+    // コントローラーから戻された内容をレスポンスとして戻す
+    echo $response;
+    exit;
+} else {
+    // ファイルがなければ404エラー
+    header("https/1.0 404 Not Found");
+}
